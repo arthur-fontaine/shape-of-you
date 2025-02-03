323277fab61f85c9891bbe489ca177971e889012
Jules Roche <julesro42@gmail.com>
2025-02-03 14:51:51 +0100

    delete useless comment and add description

--- migrations/Version20250125175009.php
+++ migrations/Version20250125175009.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20250125175009 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,7 +17,6 @@ final class Version20250125175009 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE dressing_piece ADD clothing_id INT NOT NULL');
         $this->addSql('ALTER TABLE dressing_piece ADD CONSTRAINT FK_50AD104A4CFB3290 FOREIGN KEY (clothing_id) REFERENCES clothing (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_50AD104A4CFB3290 ON dressing_piece (clothing_id)');
@@ -27,7 +24,6 @@ final class Version20250125175009 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE dressing_piece DROP CONSTRAINT FK_50AD104A4CFB3290');
         $this->addSql('DROP INDEX IDX_50AD104A4CFB3290');
--- migrations/Version20250203131152.php
+++ migrations/Version20250203131152.php
@@ -7,14 +7,11 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250203131152 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add password to User';
     }
 
     public function up(Schema $schema): void
