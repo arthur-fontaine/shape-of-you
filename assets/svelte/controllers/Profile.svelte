0153da0d919fb27d9e6794d3e929d8521c995db0
Jules Roche <julesro42@gmail.com>
2025-02-05 16:33:26 +0100

    init Profile svelte component

---
+++ assets/svelte/controllers/Profile.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<div class="min-h-screen bg-gray-100">
+    <div class="bg-white shadow-md">
+        <div class="max-w-5xl mx-auto p-4">
+            <div class="flex items-center space-x-4">
+                <img alt="Profile Picture" class="w-24 h-24 rounded-full">
+                <div>
+                    <h2 class="text-2xl font-bold">Username</h2>
+                </div>
+            </div>
+            <div class="mt-4 flex space-x-8">
+                <div>
+                    <span class="font-bold">2</span> posts
+                </div>
+                <div>
+                    <span class="font-bold">10</span> followers
+                </div>
+                    <span class="font-bold">30</span> following
+                <div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="max-w-5xl mx-auto mt-6 grid grid-cols-3 gap-4">
+            <div class="bg-white shadow-md rounded-lg overflow-hidden">
+                <img src="" alt="Post Image" class="w-full h-48 object-cover">
+                <div class="p-4">
+                    <p class="text-gray-800">text</p>
+                </div>
+            </div>
+    </div>
+</div>
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/user/{id}', name: 'app_user')]
+    public function index(User $user): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $user,
         ]);
     }
 }
