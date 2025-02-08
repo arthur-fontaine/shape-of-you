e6045c79639f7a9e57cc7d657f529de94e54b26d
Jules Roche <julesro42@gmail.com>
2025-02-08 13:59:24 +0100

    add icon

--- src/UI/pages/Profile.svelte
+++ src/UI/pages/Profile.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 
-    let props: { user?: { name: string } } = $props();
-    console.log(props);
+    export let user: {
+        name: string,
+    };
+    console.log(user.name);
 </script>
 
 <div class="min-h-screen bg-gray-100">
     <div class="bg-white shadow-md">
         <div class="max-w-5xl mx-auto p-4">
             <div class="flex items-center space-x-4">
-                <img alt="Profile Picture" class="w-24 h-24 rounded-full">
+                <img src="./image/icon_user.png" alt="profile" class="w-24 h-24 rounded-full">
                 <div>
-                    <h2 class="text-2xl font-bold">{props.user.name}</h2>
+                    <h2 class="text-2xl font-bold">{user.name}</h2>
                 </div>
             </div>
             <div class="mt-4 flex space-x-8">
@@ -28,7 +30,7 @@
     </div>
     <div class="max-w-5xl mx-auto mt-6 grid grid-cols-3 gap-4">
             <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                <img src="" alt="Post Image" class="w-full h-48 object-cover">
+                <img src="" alt="Post" class="w-full h-48 object-cover">
                 <div class="p-4">
                     <p class="text-gray-800">text</p>
                 </div>
