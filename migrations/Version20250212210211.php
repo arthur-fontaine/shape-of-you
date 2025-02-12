57322459b46f80f00435a323b9f7ed5813776c60
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-02-12 22:17:02 +0100

    Add symfony message task to calculate user recommendations

--- migrations/Version20250212210211.php
+++ migrations/Version20250212210211.php
@@ -24,13 +24,21 @@ final class Version20250212210211 extends AbstractMigration
                 LANGUAGE plpgsql
                 AS $$
                 BEGIN
-                    DELETE FROM user_clothing_recommendation;
-                    INSERT INTO user_clothing_recommendation (user_id, clothing_id)
+                    CREATE TEMP TABLE user_recommendations_tmp AS
                     SELECT id, u.clothing_id
                     FROM "user"
                     CROSS JOIN user_clothing_recommendations(id, 100) u;
 
                     RAISE NOTICE \'Recommendations calculated for % users\', (SELECT COUNT(*) FROM "user");
+
+                    DELETE FROM user_clothing_recommendation;
+                    
+                    INSERT INTO user_clothing_recommendation
+                    SELECT * FROM user_recommendations_tmp;
+
+                    DROP TABLE user_recommendations_tmp;
+
+                    RAISE NOTICE \'Recommendations recalculated for % users\', (SELECT COUNT(*) FROM user_clothing_recommendation);
                 END;
                 $$;
         ');
