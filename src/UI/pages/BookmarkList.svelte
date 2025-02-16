eb0b5b50b80a99c0602a431f86fc353a2ae39d26
Jules Roche <julesro42@gmail.com>
2025-02-16 12:56:04 +0100

    link btn bookmark list

--- src/UI/pages/BookmarkList.svelte
+++ src/UI/pages/BookmarkList.svelte
@@ -5,7 +5,6 @@
     const {user}: {
         user: IUser;
     } = $props();
-    console.log(user);
 </script>
 
 <div class="min-h-screen bg-gray-100">
@@ -13,10 +12,12 @@
     <div class="max-w-5xl mx-auto mt-6 grid grid-cols-3 gap-4">
         {#each user.clothingLists as clothingList (clothingList.id)}
             <div class="bg-white shadow-md rounded-lg overflow-hidden">
-                <img src="" alt="Bookmark" class="w-full h-48 object-cover">
-                <div class="p-4">
-                    <p class="text-gray-800">{clothingList.name}</p>
-                </div>
+                <a href="/bookmark/{clothingList.id}">
+                    <img src="" alt="Bookmark" class="w-full h-48 object-cover">
+                    <div class="p-4">
+                        <p class="text-gray-800">{clothingList.name}</p>
+                    </div>
+                </a>
             </div>
         {/each}
     </div>
