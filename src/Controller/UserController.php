1b0408d79bc82bb63f578799aca669e144d167aa
Jules Roche <julesro42@gmail.com>
2025-02-23 13:30:26 +0100

    feat: index users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\ClothingListRepository;
 use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -26,6 +27,15 @@ final class UserController extends AbstractController
         $user = $this->getUser();
         return $this->render('user/bookmark.html.twig', [
             'user' => $user
+        ]);
+    }
+
+    #[Route('/admin/users', name: 'app_admin_users')]
+    public function adminUsers(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('admin/users.html.twig', [
+            'users' => $users
         ]);
     }
 }
