be88d70ff8ec9167fe69e0035a70358aba7c21e5
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-02-25 13:51:44 +0100

    Design search page

--- src/UI/components/NavBar.svelte
+++ src/UI/components/NavBar.svelte
@@ -33,7 +33,7 @@
 
 <nav class="fixed bottom-0 left-0 w-full flex flex-col-reverse gap-6">
   <div class="border-t-border border-t-1">
-    <ul class="flex gap-4 justify-around bg-primary py-2 mx-4">
+    <ul class="flex gap-4 justify-around bg-background py-2 mx-4">
       {#each menus as menuItem}
         <li
           class={`flex-1 ${isActive(menuItem.link) ? "text-label" : "text-disabled"}`}
@@ -53,7 +53,7 @@
 
   <a
     href="/posts/new"
-    class="bg-ui-background text-ui-surface w-fit h-fit p-4 rounded-2xl flex self-end mx-6"
+    class="bg-ui-background text-ui-surface w-fit h-fit p-4 rounded-button flex self-end mx-6"
     aria-label="Créer un post"
   >
     <span class="icon-[tabler--plus] text-2xl text-white"></span>
