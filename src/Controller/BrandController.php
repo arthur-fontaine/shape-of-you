b523e82c56850d69532b2316b383b4d7ee1dccbf
Jules Roche <julesro42@gmail.com>
2025-03-05 12:06:25 +0100

    feat: getById Brand

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Brand;
 use App\Repository\BrandRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,14 @@ final class BrandController extends AbstractController
         $brands = $brandRepository->findAll();
         return $this->render('admin/brands.html.twig', [
             'brands' => $brands,
+        ]);
+    }
+
+    #[Route('/admin/brand/{id}', name: 'app_admin_brand')]
+    public function show(Brand $brand): Response
+    {
+        return $this->render('admin/brand.html.twig', [
+            'brand' => $brand,
         ]);
     }
 }
